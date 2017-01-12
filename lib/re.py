eac0280dbab6b2d4abeae1734364ef6b5ccad985
S-YOU <S-YOU@users.noreply.github.com>
2017-01-12 17:57:08 +0900

    add stdlib string

--- lib/re.py
+++ lib/re.py
@@ -13,3 +13,17 @@
 # limitations under the License.
 
 
+_alphanum = frozenset(
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+def escape(pattern):
+    "Escape all non-alphanumeric characters in pattern."
+    s = list(pattern)
+    alphanum = _alphanum
+    for i, c in enumerate(pattern):
+        if c not in alphanum:
+            if c == "\000":
+                s[i] = "\\000"
+            else:
+                s[i] = "\\" + c
+    return pattern[:0].join(s)
