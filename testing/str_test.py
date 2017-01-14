4c2966cb2ddbaae8d0b14637214dc6df2214cb36
S-YOU <S-YOU@users.noreply.github.com>
2017-01-14 12:08:48 +0000

    add str.find, str.index, and a fix

--- testing/str_test.py
+++ testing/str_test.py
@@ -28,3 +28,36 @@ assert "%%" % tuple() == "%"
 assert "%r" % "abc" == "'abc'"
 assert "%x" % 0x1f == "1f"
 assert "%X" % 0xffff == "FFFF"
+
+# Test find
+assert "".find("") == 0
+assert "".find("", 1) == -1
+assert "foobar".find("bar") == 3
+# TODO: support unicode
+# assert "foobar".find(u"bar") == 3
+assert "foobar".find("bar", 0, -2) == -1
+assert "foobar".find("foo", 0, 3) == 0
+assert "foobar".find("bar", 3, 5) == -1
+assert "foobar".find("bar", 5, 3) == -1
+assert "bar".find("foobar") == -1
+try:
+  "foo".find(123)
+except TypeError as e:
+  assert str(e) == "'find' requires a 'str' object but received a 'int'" or str(
+      e) == 'expected a string or other character buffer object'
+
+# Test index
+assert "".index("") == 0
+try:
+  "".index("", 1)
+except ValueError as e:
+  assert str(e) == "substring not found"
+assert "foobar".index("bar") == 3
+# TODO: support unicode
+# assert "foobar".find(u"bar") == 3
+assert "foobar".index("foo", 0, 3) == 0
+try:
+  "foo".index(123)
+except TypeError as e:
+  assert str(e) == "'find' requires a 'str' object but received a 'int'" or str(
+      e) == 'expected a string or other character buffer object'
