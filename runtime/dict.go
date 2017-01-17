12167e9414cf37d57a0c4aa866afeac991a3df2d
YOU <S-YOU@users.noreply.github.com>
2017-01-17 23:56:20 +0000

    Implement dict.clear() (#135)

--- runtime/dict.go
+++ runtime/dict.go
@@ -508,6 +508,18 @@ func dictsAreEqual(f *Frame, d1, d2 *Dict) (bool, *BaseException) {
 	return result, nil
 }
 
+func dictClear(f *Frame, args Args, _ KWArgs) (*Object, *BaseException) {
+	if raised := checkMethodArgs(f, "clear", args, DictType); raised != nil {
+		return nil, raised
+	}
+	d := toDictUnsafe(args[0])
+	d.mutex.Lock(f)
+	d.table = newDictTable(0)
+	d.incVersion()
+	d.mutex.Unlock(f)
+	return None, nil
+}
+
 func dictContains(f *Frame, seq, value *Object) (*Object, *BaseException) {
 	item, raised := toDictUnsafe(seq).GetItem(f, value)
 	if raised != nil {
@@ -713,6 +725,7 @@ func dictUpdate(f *Frame, args Args, kwargs KWArgs) (*Object, *BaseException) {
 }
 
 func initDictType(dict map[string]*Object) {
+	dict["clear"] = newBuiltinFunction("clear", dictClear).ToObject()
 	dict["get"] = newBuiltinFunction("get", dictGet).ToObject()
 	dict["items"] = newBuiltinFunction("items", dictItems).ToObject()
 	dict["iteritems"] = newBuiltinFunction("iteritems", dictIterItems).ToObject()
