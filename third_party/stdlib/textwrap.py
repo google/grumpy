d275f7bd98f57da91dc393e72c0d9e998a9b57a8
YOU <S-YOU@users.noreply.github.com>
2017-01-17 15:36:05 +0000

    Add optparse module to stdlib (#136)

--- third_party/stdlib/textwrap.py
+++ third_party/stdlib/textwrap.py
@@ -79,7 +79,8 @@ class TextWrapper(object):
         Drop leading and trailing whitespace from lines.
     """
 
-    whitespace_trans = string.maketrans(_whitespace, ' ' * len(_whitespace))
+    # whitespace_trans = string.maketrans(_whitespace, ' ' * len(_whitespace))
+    whitespace_trans = '\x00\x01\x02\x03\x04\x05\x06\x07\x08     \x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff'
 
     unicode_whitespace_trans = {}
     uspace = ord(u' ')
@@ -151,12 +152,14 @@ class TextWrapper(object):
         becomes " foo    bar  baz".
         """
         if self.expand_tabs:
-            text = text.expandtabs()
+            # text = text.expandtabs()
+            text = ' '.join((' '.join(text.split('\n'))).split('\t'))
         if self.replace_whitespace:
-            if isinstance(text, str):
-                text = text.translate(self.whitespace_trans)
-            elif isinstance(text, _unicode):
-                text = text.translate(self.unicode_whitespace_trans)
+            # if isinstance(text, str):
+            #     text = text.translate(self.whitespace_trans)
+            # elif isinstance(text, _unicode):
+            #     text = text.translate(self.unicode_whitespace_trans)
+            text = ' '.join(' '.join(text.split('\n')).split('\t'))
         return text
 
 
@@ -186,7 +189,8 @@ class TextWrapper(object):
             else:
                 pat = self.wordsep_simple_re
         chunks = pat.split(text)
-        chunks = filter(None, chunks)  # remove empty chunks
+        # chunks = filter(None, chunks)  # remove empty chunks
+        chunks = [x for x in chunks if x is not None]
         return chunks
 
     def _fix_sentence_endings(self, chunks):
@@ -280,7 +284,8 @@ class TextWrapper(object):
             # First chunk on line is whitespace -- drop it, unless this
             # is the very beginning of the text (ie. no lines started yet).
             if self.drop_whitespace and chunks[-1].strip() == '' and lines:
-                del chunks[-1]
+                # del chunks[-1]
+                chunks.pop()
 
             while chunks:
                 l = len(chunks[-1])
@@ -301,7 +306,8 @@ class TextWrapper(object):
 
             # If the last chunk on this line is all whitespace, drop it.
             if self.drop_whitespace and cur_line and cur_line[-1].strip() == '':
-                del cur_line[-1]
+                # del cur_line[-1]
+                cur_line.pop()
 
             # Convert current line back to a string and store it in list
             # of all lines (return value).
