88e43ac0f91344dcd3cd0c6ba95cd121c1f7a681
Dylan Trotter <trotterdylan@users.noreply.github.com>
2017-01-17 21:06:58 -0800

    Fix continue/break in else block behavior (#146)
    Fixes: https://github.com/google/grumpy/issues/123

--- testing/for_test.py
+++ testing/for_test.py
@@ -51,3 +51,26 @@ for i, j in [('a', 1), ('b', 2)]:
   l.append(i)
   l.append(j)
 assert l == ['a', 1, 'b', 2]
+
+# break and continue statements in an else clause applies to the outer loop.
+# See: https://github.com/google/grumpy/issues/123
+l = []
+for i in range(2):
+  l.append(i)
+  for j in range(10, 12):
+    l.append(j)
+  else:
+    l.append(12)
+    continue
+  l.append(-1)
+assert l == [0, 10, 11, 12, 1, 10, 11, 12]
+
+l = []
+for i in range(10):
+  l.append(i)
+  for j in range(10, 12):
+    l.append(j)
+  else:
+    break
+  l.append(-1)
+assert l == [0, 10, 11]
