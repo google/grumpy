c02017d808824f168f12b8c8d68548be80fbfb76
YOU <S-YOU@users.noreply.github.com>
2017-01-27 01:23:41 +0000

    Add LockType, and allocate_lock to thread.py (#218)

--- lib/thread.py
+++ lib/thread.py
@@ -1,5 +1,30 @@
+from __go__.sync import type_Mutex as Mutex
+
+
 def get_ident():
   f = __frame__()
   while f.f_back:
     f = f.f_back
   return id(f)
+
+
+class LockType(object):
+  def __init__(self):
+    self._mutex = Mutex.new()
+
+  def acquire(self):
+    self._mutex.Lock()
+
+  def release(self):
+    self._mutex.Unlock()
+
+  def __enter__(self):
+    self.acquire()
+
+  def __exit__(self, *args):
+    self.release()
+
+
+def allocate_lock():
+    """Dummy implementation of thread.allocate_lock()."""
+    return LockType()
