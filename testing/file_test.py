67142cd43b712be4e3cefc1c0f6c9c106beb25d8
Alan Justino <alan.justino@yahoo.com.br>
2017-04-05 18:47:38 -0300

    Test the 'file.softspace' attribute

---
+++ testing/file_test.py
@@ -0,0 +1,10 @@
+f = open('/tmp/foo', 'w')
+assert f.softspace == 0
+
+f.softspace = 1
+assert f.softspace == 1
+
+try:
+    f.softspace = 'deadbeef'
+except TypeError as e:
+    assert str(e) == "an int is required. Field 'file.softspace' cannot store a 'str' value"
