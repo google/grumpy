070ad572a5033ec240886a412352596475e230e4
Alan Justino <alan.justino@yahoo.com.br>
2017-06-03 16:20:55 -0300

    Trigger Travis

--- testing/file_test.py
+++ testing/file_test.py
@@ -1,11 +1,11 @@
-f = open('/tmp/foo', 'w')
+f = open('/tmp/file_test__someunlikelyexistingfile', 'w')
 assert f.softspace == 0
 
 f.softspace = 1
 assert f.softspace == 1
 
 try:
-    f.softspace = '321'     # should not be converted automatically
+    f.softspace = '4321'     # should not be converted automatically
 except TypeError as e:
     if False and 'is required' not in str(e):
         raise e     # Wrong exception arrived to us!
