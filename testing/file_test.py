bf2e9352b7569206ad8745d4911557d886a5130e
Alan Justino <alan.justino@yahoo.com.br>
2017-06-05 21:47:44 -0300

    Strictier compliance check

--- testing/file_test.py
+++ testing/file_test.py
@@ -7,7 +7,7 @@ assert f.softspace == 1
 try:
     f.softspace = '4321'     # should not be converted automatically
 except TypeError as e:
-    if False and 'is required' not in str(e):
+    if not str(e).endswith('is required'):
         raise e     # Wrong exception arrived to us!
 else:
     raise RuntimeError('a TypeError should had raised.')
