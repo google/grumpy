6ed2beaad5ed6120900f26081076dbad1391c62c
Mansour Rahimi <rahimi.mnr@gmail.com>
2017-07-22 13:11:54 +0200

    Revert "Refactor putItem to accept operation by a func"
    This reverts commit d6355e2a4fad8c085ca081d8bbc874eaa8303e55.

--- runtime/dict.go
+++ runtime/dict.go
@@ -326,7 +326,7 @@ func (d *Dict) incVersion() {
 // DelItem removes the entry associated with key from d. It returns true if an
 // item was removed, or false if it did not exist in d.
 func (d *Dict) DelItem(f *Frame, key *Object) (bool, *BaseException) {
-	originValue, raised := d.putItem(f, key, nil, deleteDictEntryFunc)
+	originValue, raised := d.putItem(f, key, nil)
 	if raised != nil {
 		return false, raised
 	}
@@ -365,7 +365,7 @@ func (d *Dict) GetItemString(f *Frame, key string) (*Object, *BaseException) {
 // Pop looks up key in d, returning and removing the associalted value if exist,
 // or nil if key is not present in d.
 func (d *Dict) Pop(f *Frame, key *Object) (*Object, *BaseException) {
-	return d.putItem(f, key, nil, deleteDictEntryFunc)
+	return d.putItem(f, key, nil)
 }
 
 // Keys returns a list containing all the keys in d.
@@ -390,8 +390,7 @@ func (d *Dict) Len() int {
 
 // putItem associates value with key in d, returning the old associated value if
 // the key was added, or nil if it was not already present in d.
-// fun is defining the operation on d, which can be add or delete the entry.
-func (d *Dict) putItem(f *Frame, key, value *Object, fun func(f *Frame, d *Dict, entry *dictEntry, hash *Int, key, value *Object, index int) *BaseException) (*Object, *BaseException) {
+func (d *Dict) putItem(f *Frame, key, value *Object) (*Object, *BaseException) {
 	hash, raised := Hash(f, key)
 	if raised != nil {
 		return nil, raised
@@ -407,7 +406,16 @@ func (d *Dict) putItem(f *Frame, key, value *Object, fun func(f *Frame, d *Dict,
 			// of trying to recover.
 			raised = f.RaiseType(RuntimeErrorType, "dictionary changed during write")
 		} else {
-			raised = fun(f, d, entry, hash, key, value, index)
+			if value == nil {
+				// Going to delete the entry.
+				if entry != nil && entry != deletedEntry {
+					d.table.storeEntry(index, deletedEntry)
+					d.table.incUsed(-1)
+					d.incVersion()
+				}
+			} else {
+				raised = addDictTableEntry(f, d, hash, key, value, index)
+			}
 			if entry != nil && entry != deletedEntry {
 				originValue = entry.value
 			}
@@ -419,7 +427,7 @@ func (d *Dict) putItem(f *Frame, key, value *Object, fun func(f *Frame, d *Dict,
 
 // SetItem associates value with key in d.
 func (d *Dict) SetItem(f *Frame, key, value *Object) *BaseException {
-	_, raised := d.putItem(f, key, value, addDictEntryFunc)
+	_, raised := d.putItem(f, key, value)
 	return raised
 }
 
@@ -754,23 +762,36 @@ func dictSetDefault(f *Frame, args Args, _ KWArgs) (*Object, *BaseException) {
 	if raised := checkMethodArgs(f, "setdefault", args, expectedTypes...); raised != nil {
 		return nil, raised
 	}
-	d := toDictUnsafe(args[0])
 	key := args[1]
+	d := toDictUnsafe(args[0])
+	hash, raised := Hash(f, key)
+	if raised != nil {
+		return nil, raised
+	}
+	d.mutex.Lock(f)
+	t := d.table
+	v := d.version
+	index, entry, raised := t.lookupEntry(f, hash.Value(), key)
 	var value *Object
-	if argc > 2 {
-		value = args[2]
-	} else {
-		value = None
-	}
-	origValue, raised := d.putItem(f, key, value, func(f *Frame, d *Dict, entry *dictEntry, hash *Int, key, value *Object, index int) *BaseException {
-		if entry == nil {
-			return addDictEntryFunc(f, d, nil, hash, key, value, index)
+	if raised == nil {
+		if v != d.version {
+			// Dictionary was recursively modified. Blow up instead
+			// of trying to recover.
+			raised = f.RaiseType(RuntimeErrorType, "dictionary changed during write")
+		} else {
+			if entry == nil {
+				if argc > 2 {
+					value = args[2]
+				} else {
+					value = None
+				}
+				raised = addDictTableEntry(f, d, hash, key, value, index)
+			} else {
+				value = entry.value
+			}
 		}
-		return nil
-	})
-	if origValue != nil {
-		return origValue, raised
 	}
+	d.mutex.Unlock(f)
 	return value, raised
 }
 
@@ -839,9 +860,9 @@ func initDictType(dict map[string]*Object) {
 	DictType.slots.SetItem = &setItemSlot{dictSetItem}
 }
 
-// addDictEntryFunc adds a new entry in d. It assumes that d.mutex is held by
+// addDictTableEntry adds a new entry in d. It assumes that d.mutex is held by
 // the caller.
-func addDictEntryFunc(f *Frame, d *Dict, entry *dictEntry, hash *Int, key, value *Object, index int) *BaseException {
+func addDictTableEntry(f *Frame, d *Dict, hash *Int, key, value *Object, index int) *BaseException {
 	newEntry := &dictEntry{hash.Value(), key, value}
 	if newTable, ok := d.table.writeEntry(f, index, newEntry); ok {
 		if newTable != nil {
@@ -851,17 +872,6 @@ func addDictEntryFunc(f *Frame, d *Dict, entry *dictEntry, hash *Int, key, value
 		return nil
 	}
 	return f.RaiseType(OverflowErrorType, errResultTooLarge)
-}
-
-// deleteDictEntryFunc deletes the entry from d. It assumes that d.mutex is
-// held by the caller.
-func deleteDictEntryFunc(f *Frame, d *Dict, entry *dictEntry, hash *Int, key, value *Object, index int) *BaseException {
-	if entry != nil && entry != deletedEntry {
-		d.table.storeEntry(index, deletedEntry)
-		d.table.incUsed(-1)
-		d.incVersion()
-	}
-	return nil
 }
 
 type dictItemIterator struct {
