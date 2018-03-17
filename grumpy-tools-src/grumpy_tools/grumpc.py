4e4db780c05bb4dbcea31523360c5faef040a881
Alan Justino <alan.justino@yahoo.com.br>
2018-03-17 17:53:55 -0300

    tools grump{c,run} should be called via CLI:
    grumpc -> grumpy translate
    grumprun -> grumpy run

--- grumpy-tools-src/grumpy_tools/grumpc.py
+++ grumpy-tools-src/grumpy_tools/grumpc.py
@@ -32,7 +32,6 @@ from .vendor import pythonparser
 
 
 def main(script=None, modname=None):
-  # import wdb; wdb.set_trace()
   assert script and modname, 'Script "%s" or Modname "%s" is empty' % (script,modname)
 
   gopath = os.getenv('GOPATH', None)
